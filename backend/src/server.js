3a1c2960839db29ce5c1755c90c6d7d1872ea3bb
Muhammad5Ali <mohammad.bscs4490@iiu.edu.pk>
2025-11-21 10:59:15 +0500

    server side auth implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,24 +4,31 @@ import path from "path";
 import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { serve } from "inngest/express";
+import { clerkMiddleware } from '@clerk/express'
 import { inngest, functions } from "./lib/inngest.js";
+// import { protectRoute } from './middleware/protectRoute.js';
+import chatRoutes from './routes/chatRoutes.js';
 
 const app = express();
 const __dirname = path.resolve();
-
 //middlewares
 app.use(express.json());
 // credentials: true :-> server allows browser(frontend) to include cookie in requests
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-
-
+app.use(clerkMiddleware()); //this adds the Clerk middleware to handle authentication
+//add auth field to the request object: req.auth()
 app.use("/api/inngest", serve({ client: inngest, functions }));
-
-console.log('Environment Variables:', ENV.PORT)
+app.use('/api/chats', chatRoutes);
 
 app.get('/health', (req, res) => {
    res.status(200).json({ message: 'Api is up & running' });
 });
+
+//when you pass an array of middleware to express, it automatically flattens and execute them sequentially,
+//one by one
+// app.get('/video-calls', protectRoute, (req, res) => {
+//    res.status(200).json({ message: 'Video Call Api is up & running' });
+// });
 
 // make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
